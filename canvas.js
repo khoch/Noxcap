310cf77052fb602c7e6c78dacd7baee3654e91ef
Krzysztof Hochlewicz <khochlewicz@stuy.edu>
2016-03-11 08:34:44 -0500

    I still believe we're going to finish Noxcap.

--- canvas.js
+++ canvas.js
@@ -1,32 +1,33 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-var mysvg = document.getElementById("mysvg");
+var mysvg = document.getElementById("pacman");
 
-//ctx.fillStyle = "#000ff";
-//ctx.fillRect(50,50,100,200);
-//ctx.beginPath();
-//ctx.arc(200,300,50,0,Math.PI*2);
-//ctx.fill();
+var pacleft = false;
+var pacdown = true;
+var pacup = false;
+var pacright = false;
 
-//ignore part 2
-//ctx.beginPath();
-//ctx.moveTo(250,250);
-//ctx.quadraticCurveTo(250,250,400,250);
-//ctx.quadraticCurveTo(250, 250, 200, 300);
-//ctx.closePath();
-//ctx.stroke();
-//ctx.fill();
-
-
-
-var left = false;
-var down = true;
-var up = false;
-var right = false;
 var spacedown = false;
 
-var x = 10;
-var y = 10;
+
+var ghostdown = true;
+
+var ghostright = true;
+
+var ghostHBtopX;
+var ghostHBtopY;
+var ghostHBbotX;
+var ghostHBbotY;
+var ghostHBleftX;
+var ghostHBleftY;
+var ghostHBrightX;
+var ghostHBrightY;
+
+var ghostx = 200;
+var ghosty = 250;
+
+var pacx = 10;
+var pacy = 10;
 
 ctx.rect(10, 10, 480, 480);
 ctx.fillStyle = 'black';
@@ -37,36 +38,46 @@ ctx.stroke();
 
 var start = function start(){
 	//ctx.clearRect(0,0,500,500);
-	
-	if (left && x > 10){
-		x -= 10;
+	if (pacleft && pacx > 10){
+		pacx -= 10;
 	}
-	else if (right && x < 490){
-		x += 10;
+	else if (pacright && pacx < 490){
+		pacx += 10;
 	}
-	else if (down && y < 490){
-		y += 10;
+	else if (pacdown && pacy < 490){
+		pacy += 10;
 
 	}
-	else if (up && y > 10){
-		y -= 10;
+	else if (pacup && pacy > 10){
+		pacy -= 10;
 	}
 	if (spacedown){
-	ctx.lineTo(x,y);
+	ctx.lineTo(pacx,pacy);
 	ctx.stroke();
 	}
-	//ctx.moveTo(x,y);
-	//ctx.beginPath();
-	//ctx.moveTo(x,y);
-	//ctx.strokeStyle = 'blue';
-	//ctx.fillStyle = 'blue';
-	//ctx.lineWidth = 1;
-	//ctx.beginPath();
-	//ctx.arc(x, y, 1, 0, Math.PI*2);
-	//ctx.stroke();
-	//ctx.fill();
-	mysvg.style.left = x-10 + "px";
-	mysvg.style.top = y-10 + "px";
+
+	pacman.style.left = pacx-10 + "px";
+	pacman.style.top = pacy-10 + "px";
+	
+	if(ghostdown){
+		ghosty += 5;
+	}
+	else{
+		ghosty -= 5;
+	}
+	if(ghostright){
+		ghostx += 5;
+	}
+	else{
+		ghostx -= 5;
+	}
+	
+	ghost.style.left = ghostx-10 + "px";
+	ghost.style.top = ghosty-10 + "px";
+	
+	var p = ctx.getImageData(pacx, pacy, 1, 1).data; 
+    //var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+	//console.log(p);
 };
 
 document.onkeydown = checkKey;
@@ -77,31 +88,31 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
         // up arrow
-        left = false;
-	down = false;
-	up = true;
-	right = false;
+        pacleft = false;
+		pacdown = false;
+		pacup = true;
+		pacright = false;
     }
     else if (e.keyCode == '40') {
         // down arrow
-        left = false;
-	down = true;
-	up = false;
-	right = false;
+        pacleft = false;
+		pacdown = true;
+		pacup = false;
+		pacright = false;
     }
     else if (e.keyCode == '37') {
        // left arrow
-	left = true;
-	down = false;
-	up = false;
-	right = false;
+		pacleft = true;
+		pacdown = false;
+		pacup = false;
+		pacright = false;
     }
     else if (e.keyCode == '39') {
        // right arrow
-        left = false;
-	down = false;
-	up = false;
-	right = true;
+        pacleft = false;
+		pacdown = false;
+		pacup = false;
+		pacright = true;
     }
 	else if (e.keyCode == '32') {
        // space bar
@@ -119,7 +130,5 @@ function checkKey(e) {
     }
 
 };
-
-
 
 myInterval = setInterval(start, 40);
